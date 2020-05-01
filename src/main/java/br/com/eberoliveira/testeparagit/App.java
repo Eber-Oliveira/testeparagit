fed6241fd36705bade72e2609c712f7f84c7ee95
Eber de Oliveira Costa <38170848+Eber-Oliveira@users.noreply.github.com>
2020-05-01 16:49:46 -0300

    Arquivos iniciais modificados

--- src/main/java/br/com/eberoliveira/testeparagit/App.java
+++ src/main/java/br/com/eberoliveira/testeparagit/App.java
@@ -8,6 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+     
+    	System.out.println( "Contador");
+    	int n = 10;
+    	
+    	for (int i = 0; i < n ; i++) {
+    	
+    		System.out.println(i);
+    	}
+    	
     }
 }
