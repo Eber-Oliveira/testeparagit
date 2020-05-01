2fc9bf7af2377ffddb9f07ec7e3c487785302fae
Eber Oliveira <eber.oliv.costa@gmail.com>
2020-05-01 17:17:42 -0300

    Adiciona mensagem para contador e classe funcionario

--- src/main/java/br/com/eberoliveira/testeparagit/App.java
+++ src/main/java/br/com/eberoliveira/testeparagit/App.java
@@ -6,16 +6,17 @@ package br.com.eberoliveira.testeparagit;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-     
-    	System.out.println( "Contador");
+    public static void main( String[] args ){
     	int n = 10;
+    	
+    	System.out.println( "Contador até n");
     	
     	for (int i = 0; i < n ; i++) {
     	
     		System.out.println(i);
     	}
+    	
+    	System.out.println("Finalizado");
     	
     }
 }
